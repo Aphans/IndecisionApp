109e9add3b6bc87ba280335912efec29a425abdd
Ángel <54036253+Aphans@users.noreply.github.com>
2023-01-24 22:58:38 +0100

    Fixing answer

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -10,7 +10,7 @@
         <p>Recuerda terminar con un signo de interrogación (?)</p>
         <div v-if="isValidQuestion">
             <h2>{{question}}</h2>
-            <h1>{{answer==="yes"?'Si!':'No'}}</h1>
+            <h1>{{answer==="yes"?'Si!':{{answer}}}}</h1>
         </div>
   </div>
 </template>
